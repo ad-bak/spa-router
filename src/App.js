41560a68d47c9a96653f329020e5de627065f664
Adnan <adbak.dev@gmail.com>
2022-12-18 15:54:16 +0100

    added pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { Route, Switch, Redirect } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+
+import AllQuotes from "./pages/AllQuotes";
+import NewQuote from "./pages/NewQuote";
+import QuoteDetail from "./pages/QuoteDetail";
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
+        <Route exact path="/quotes">
+          <AllQuotes />
+        </Route>
+        <Route path="/quotes/:quoteId">
+          <QuoteDetail />
+        </Route>
+        <Route path="/new-quote">
+          <NewQuote />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,18 @@
+import { useParams, Route } from "react-router-dom";
+
+import Comments from "../components/comments/Comments";
+
+const QuoteDetail = () => {
+  const params = useParams();
+  return (
+    <>
+      <h1>Quote detail page</h1>
+      <p>{params.quoteId}</p>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </>
+  );
+};
+
+export default QuoteDetail;
