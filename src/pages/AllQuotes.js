cfe29b00b70efea6213696fbee6b564a4af1bdef
Adnan <adbak.dev@gmail.com>
2022-12-22 23:43:46 +0100

    added firebase API

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,4 +1,9 @@
+import { useEffect } from "react";
 import QuoteList from "../components/quotes/QuoteList";
+
+import NoQuotesFound from "../components/quotes/NoQuotesFound";
+import useHttp from "../hooks/use-http";
+import { getAllQuotes } from "../lib/api";
 
 const DUMMY_QUOTES = [
   { id: "q1", author: "Max", text: "Learning React is fun!" },
@@ -6,11 +11,38 @@ const DUMMY_QUOTES = [
 ];
 
 const AllQuotes = () => {
-  return (
-    <div>
-      <QuoteList quotes={DUMMY_QUOTES} />
-    </div>
-  );
+  const {
+    sendRequest,
+    status,
+    data: loadedQuotes,
+    error,
+  } = useHttp(getAllQuotes, true);
+
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="centered">
+        <p>{error}</p>
+      </div>
+    );
+  }
+
+  if (status === "completed" && (!loadedQuotes || loadedQuotes.length === 0)) {
+    return <NoQuotesFound />;
+  }
+
+  return <QuoteList quotes={loadedQuotes} />;
 };
 
 export default AllQuotes;
